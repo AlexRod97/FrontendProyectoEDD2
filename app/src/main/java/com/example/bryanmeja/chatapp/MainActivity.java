34af5e2c7cd794535bc23f3517897d17dd959fea
AlexRod97 <rodriguezalex86@hotmail.com>
2018-11-23 22:53:26 -0600

    Login modified

--- app/src/main/java/com/example/bryanmeja/chatapp/MainActivity.java
+++ app/src/main/java/com/example/bryanmeja/chatapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.bryanmeja.chatapp.services.API;
@@ -12,8 +13,9 @@ import com.example.bryanmeja.chatapp.services.API;
 import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
-    TextView btnOk;
-    TextView btnRegister;
+    TextView btnOk, btnRegister;
+    EditText etEmail, etPassword;
+    String email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +24,15 @@ public class MainActivity extends AppCompatActivity {
 
         btnOk = (TextView) findViewById(R.id.tvLogin);
         btnRegister = (TextView) findViewById(R.id.tvRegister);
+        etEmail = (EditText)findViewById(R.id.etEmail);
+        etPassword = (EditText) findViewById(R.id.etPassword);
 
         btnOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                email = etEmail.getText().toString();
+                password = etPassword.getText().toString();
+
                 Intent intent = new Intent(MainActivity.this, lista_chat.class);
                 startActivity(intent);
             }
